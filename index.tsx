701633dc7f9b61a9c5a3c65597b79c459e71399f
KarlaKeurin <karlakeurinoliveira@gmail.com>
2024-08-29 23:52:30 -0300

    Updates

---
+++ index.tsx
@@ -0,0 +1,61 @@
+import instagramIcon from './assets/social-media-icons/instagram-icon.svg';
+import githubIcon from './assets/social-media-icons/github-icon.svg';
+import linkedinIcon from './assets/social-media-icons/linkedin-icon.svg';
+import emailIcon from './assets/social-media-icons/email-icon.svg';
+import shineIcon from './assets/images/shine-icon.png';
+import photoAbout from './assets/images/photo-about.png';
+import javascript from './assets/skills-icons/icon-javascript.svg';
+import typescript from './assets/skills-icons/icon-typescript.svg';
+import python from './assets/skills-icons/icon-python.svg';
+import csharp from './assets/skills-icons/icon-csharp.svg';
+import figma from './assets/skills-icons/icon-figma.svg';
+import nodejs from './assets/skills-icons/icon-nodejs.svg';
+import sequelize from './assets/skills-icons/icon-sequelize.svg';
+import docker from './assets/skills-icons/icon-docker.svg';
+import mysql from './assets/skills-icons/icon-mysql.svg';
+import html from './assets/skills-icons/icon-html.svg';
+import css from './assets/skills-icons/icon-css.svg';
+import react from './assets/skills-icons/icon-react.svg';
+import arrowIcon from './assets/social-media-icons/arrow-icon.svg';
+import starRight from './assets/projects-images/star-right.png';
+import shineRight from './assets/projects-images/shine-right.png';
+import starLeft from './assets/projects-images/star-left.png';
+import shineLeft from './assets/projects-images/shine-left.png';
+import tfc from './assets/projects-images/tfc.png';
+import atelierCouture from './assets/projects-images/atelier-couture.png';
+import trybeHotel from './assets/projects-images/trybe-hotel.png';
+import menu from './assets/social-media-icons/menu.svg';
+import closeMenu from './assets/social-media-icons/close-menu.svg';
+import tastemade from './assets/projects-images/tastemade.png'
+
+export {
+  instagramIcon,
+  githubIcon,
+  linkedinIcon,
+  emailIcon,
+  shineIcon,
+  photoAbout,
+  javascript,
+  typescript,
+  python,
+  csharp,
+  figma,
+  nodejs,
+  sequelize,
+  docker,
+  mysql,
+  html,
+  css,
+  react,
+  arrowIcon,
+  starRight,
+  shineRight,
+  starLeft,
+  shineLeft,
+  tfc,
+  atelierCouture,
+  trybeHotel,
+  menu,
+  closeMenu,
+  tastemade,
+};
